2d69a6860306a6863076bdcfb1d41849adb2a2d2
mizupe <82708314+fumifumi0831@users.noreply.github.com>
2025-03-10 11:30:29 +0900

    Azure用のTerraform設定ファイルを追加

---
+++ azure/main.tf
@@ -0,0 +1,47 @@
+# azure/main.tf
+
+# Azureプロバイダー設定
+provider "azurerm" {
+  features {}
+}
+
+# リソース名の重複を避けるためのランダム文字列
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  lower   = true
+  upper   = false
+}
+
+# リソースグループを作成
+resource "azurerm_resource_group" "example" {
+  name     = "${var.resource_group_prefix}-${random_string.random.result}"
+  location = var.azure_location
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
+
+# ストレージアカウントを作成
+resource "azurerm_storage_account" "example" {
+  name                     = "${var.storage_account_prefix}${random_string.random.result}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
+
+# ストレージコンテナを作成
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
