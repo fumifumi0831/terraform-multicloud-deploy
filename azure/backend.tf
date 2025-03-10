3ff28c7c08a1842e87806fed7eef1522d9ada0cf
fumiya <tonahuma77@gmail.com>
2025-03-10 18:43:10 +0900

    azureのgithubactionsの設定

--- azure/backend.tf
+++ azure/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstate12345"  # ユニークな名前に置き換える
+    storage_account_name = "terraformstate123test"  # ユニークな名前に置き換える
     container_name       = "tfstate"
     key                  = "azure/terraform.tfstate"
   }
