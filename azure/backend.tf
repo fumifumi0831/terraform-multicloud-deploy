03415c00891a74c829a39a6015cb901218c3e361
mizupe <82708314+fumifumi0831@users.noreply.github.com>
2025-03-10 11:30:54 +0900

    Azure用のバックエンド設定ファイルを追加

---
+++ azure/backend.tf
@@ -0,0 +1,10 @@
+# azure/backend.tf
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "terraformstate12345"  # ユニークな名前に置き換える
+    container_name       = "tfstate"
+    key                  = "azure/terraform.tfstate"
+  }
+}
